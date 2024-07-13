e27397608fbd73a2dbe2a9e9d9d43935435cf621
ChunRed <linjack9001155@gmail.com>
2024-07-13 22:55:27 +0800

    add model 3 & 4

--- Assets/UI/UI_ctrl.cs
+++ Assets/UI/UI_ctrl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using  TMPro;
+using UnityEngine.UI;
 
 public class UI_ctrl : MonoBehaviour
 {
@@ -15,9 +16,19 @@ public class UI_ctrl : MonoBehaviour
     public string tracker;
     public TextMeshProUGUI tracker_content;
     public TextMeshProUGUI tracker_title;
+
+
+    //光線角度
+    public Slider slider;
+    public GameObject light;
     
     void Start() {
         state.text = "state : lost tracker";
+
+        slider.onValueChanged.AddListener((value) =>{
+            //Debug.Log(value);
+            light.transform.rotation = Quaternion.Euler(20, 360f * value, 0);
+        });
     }
 
     void Update(){
@@ -27,13 +38,13 @@ public class UI_ctrl : MonoBehaviour
     void TrackerContent(){
         switch(tracker){
             case "01":
-                tracker_title.text = "Seed 01 : 椅子";
-                tracker_content.text = "這是一張衣子。";
+                tracker_title.text = "Seed 01 : 茄苳樹的由來";
+                tracker_content.text = "一棵茄苳樹的種子從樹上墜落，被一隻鳥叼起飛向天空。在翅膀拍動過程中，獨自落下，最後掉入草叢中。經過日曬雨淋，長出枝枒，慢慢茁壯成一棵樹冠高大，枝繁葉茂的大樹。";
                 break;
 
             case "02":
                 tracker_title.text = "Seed 02 : 白猿批衣";
-                tracker_content.text = "白猿正在與阿寶爭執該如何喬裝成人藉此獲得劇組道具，然而雙方爭論不休，白猿決定離開部餐與阿寶的計畫，此時阿寶的姑姑現身拉住白猿，姑姑替白猿披上縫好的衣服。";
+                tracker_content.text = "白猿正在與阿寶爭執該如何喬裝成人，藉此獲得劇組道具，然而雙方爭論不休，計畫遲遲未定。白猿決定離開拒絕餐與阿寶的計畫。此時，阿寶的姑姑現身拉住白猿，並替白猿披上縫好的衣服。";
                 break;
 
             case "03":
--- Assets/_Demo/Scripts/PlayAnimationOnOffTracking.cs
+++ Assets/_Demo/Scripts/PlayAnimationOnOffTracking.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlayAnimationOnOffTracking : NYImageTrackerEventHandler
 {
-    public Animator targetAnim;
+    // public Animator targetAnim;
+    // public Animator targetAnim2;
+
+    public List<Animator> listAnim = new List<Animator>();
 
     public string onFoundAnimName;
     public string onLostAnimName;
 
+    // public string onFoundAnimName2;
+    // public string onLostAnimName2;
+
     public override void OnTrackingFound()
     {
-        targetAnim.Play(onFoundAnimName);
+        for(int i=0; i < listAnim.Count; i++){
+            listAnim[i].Play(onFoundAnimName);
+        }
+        // targetAnim.Play(onFoundAnimName);
+        // targetAnim2.Play(onFoundAnimName2);
     }
 
     public override void OnTrackingLost()
     {
-        targetAnim.Play(onLostAnimName);
+        for(int i=0; i < listAnim.Count; i++){
+            listAnim[i].Play(onLostAnimName);
+        }
+        // targetAnim.Play(onLostAnimName);
+        // targetAnim2.Play(onLostAnimName2);
     }
 }
